6896c57b0ac383807a77e3d29a24590bf4cda65f
phuongthao2145 <thaobtp@tdc.edu.vn>
2022-04-01 13:49:10 +0700

    initial commit

---
+++ wp-content/themes/mytheme/index.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+if( $wp_query->have_posts() ) {
+    while( $wp_query->have_posts() ) {
+        $wp_query->the_post();
+        echo the_title('<h1>','</h1>');
+        echo "<p>".the_content()."</p>";
+    }
+  }
+
+?>
+</pre>
